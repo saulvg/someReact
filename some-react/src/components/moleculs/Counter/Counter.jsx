433e3ff17c740af5d3e094126092c1c79a4d865b
saulvg <saulvgproyecto@gmail.com>
2024-06-08 16:26:24 +0200

    install react-icons

--- some-react/src/components/moleculs/Counter/Counter.jsx
+++ some-react/src/components/moleculs/Counter/Counter.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import { Button } from "../../atoms";
+import { HiPlus } from "react-icons/hi";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
+    <div className="">
       <Button onClick={() => setCount(count + 1)}>-</Button>
       <p>{count}</p>
-      <Button>+</Button>
+      <Button>
+        <HiPlus />
+      </Button>
     </div>
   );
 };
